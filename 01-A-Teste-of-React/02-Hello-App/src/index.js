7d576e94a8902972572835fa4447e8d70d938c26
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-10-25 03:37:37 +0700

    finished hellp app

---
+++ 01-A-Teste-of-React/02-Hello-App/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles.css';
+
+class Hello extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello there!</h1>
+                <h1>Hello there!</h1>
+                <h1>Hello there!</h1>
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
