eeb9999ed4619a505e26031d482a39fb58b2b305
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-11-19 11:55:31 +0700

    finished section 7

---
+++ 07-State-Dice/dice/src/Die.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Die.css';
+
+class Die extends Component {
+  render() {
+    return (
+      <div className="Die">
+        <i
+          className={`fas fa-dice-${this.props.face} ${this.props.rolling &&
+            'shaking'}`}
+        />
+      </div>
+    );
+  }
+}
+
+export default Die;
---
+++ 07-State-Dice/notes.md
@@ -0,0 +1,2 @@
+# React State Dice Exercise
+
