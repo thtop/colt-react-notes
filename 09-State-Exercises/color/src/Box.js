4dc5d4f22aa4442cd3a13b8141540985c378cc94
Thamonwan Maneechan <thamonwan.top@gmail.com>
2019-11-19 17:48:08 +0700

    finished section 9

---
+++ 09-State-Exercises/color/src/Box.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { choice } from './helpers';
+import './Box.css';
+
+class Box extends Component {
+  static defaultProps = {
+    allColors: ['purple', 'magenta', 'violet', 'pink']
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { color: choice(this.props.colors) };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  pickColor() {
+    let newColor;
+
+    do {
+      newColor = choice(this.props.colors);
+    } while (newColor === this.state.color);
+
+    this.setState({ color: newColor });
+  }
+
+  handleClick() {
+    this.pickColor();
+  }
+
+  render() {
+    return (
+      <div
+        className="Box"
+        style={{ backgroundColor: this.state.color }}
+        onClick={this.handleClick}
+      ></div>
+    );
+  }
+}
+
+export default Box;
